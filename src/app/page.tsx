2fc8ac3f751871a3c3226f37d583bd15de6ba4f3
Santino <santinoaguirre2004@gmail.com>
2025-09-04 01:11:10 -0300

    feat(benefits): add mobile layout for benefits section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { RiSearch2Line } from "react-icons/ri";
 import Image from "next/image";
 import TestimonialCard from "@/components/cards/testimonialCard.tsx/testimonialCard";
 import SecondaryButton from "@/components/buttons/secondaryButton";
+import BenefitCard from "@/components/cards/benefitCard/benefitCard";
 
 export default function Home() {
   return (
@@ -119,7 +120,88 @@ export default function Home() {
         />
       </div>
 
-      
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mt-16">
+        <BenefitCard
+          title="Property Insurance"
+          text="We offer our customer property protection of liability coverage and insurance for their better life."
+          icon={
+            <Image
+              src={"/icons/propertyCheck.svg"}
+              alt="Property Insurance"
+              width={32}
+              height={32}
+            />
+          }
+          subIcon={
+            <Image
+              src={"/icons/insurance.svg"}
+              alt="Insurance"
+              width={32}
+              height={32}
+            />
+          }
+        />
+        <BenefitCard
+          title="Best Price"
+          text="Not sure what you should be charging for your property? No need to worry, let us do the numbers for you."
+          icon={
+            <Image
+              src={"/icons/ticketPrice.svg"}
+              alt="Best Price"
+              width={32}
+              height={32}
+            />
+          }
+          subIcon={
+            <Image
+              src={"/icons/discount.svg"}
+              alt="Best Price"
+              width={32}
+              height={32}
+            />
+          }
+        />
+        <BenefitCard
+          title="Lowest Commission"
+          text="You no longer have to negotiate commissions and haggle with other agents it only cost 2%!"
+          icon={
+            <Image
+              src={"/icons/lowCommission.svg"}
+              alt="Lowest Commission"
+              width={32}
+              height={32}
+            />
+          }
+          subIcon={
+            <Image
+              src={"/icons/price.svg"}
+              alt="Lowest Commission"
+              width={32}
+              height={32}
+            />
+          }
+        />
+        <BenefitCard
+          title="Overall Control"
+          text="Get a virtual tour, and schedule visits before you rent or buy any properties. You get overall control."
+          icon={
+            <Image
+              src={"/icons/locationControl.svg"}
+              alt="Overall Control"
+              width={32}
+              height={32}
+            />
+          }
+          subIcon={
+            <Image
+              src={"/icons/control.svg"}
+              alt="Overall Control"
+              width={32}
+              height={32}
+            />
+          }
+        />
+      </div>
     </main>
   );
 }
---
+++ src/components/cards/benefitCard/benefitCard.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ReactNode } from "react";
+
+type BenefitCardProps = {
+  title: string;
+  text: string | ReactNode;
+  icon: ReactNode;
+  subIcon: ReactNode;
+};
+
+export default function BenefitCard({
+  title,
+  text,
+  icon,
+  subIcon,
+}: BenefitCardProps) {
+  return (
+    <div className="flex flex-row lg:flex-col items-start text-left">
+      <div className="border border-shadowPrimary rounded-full">
+        <div className="relative flex items-center justify-center w-16 h-16 rounded-full border-2 border-white bg-shadowPrimary">
+          <span className="text-primary text-2xl">{icon}</span>
+
+          <div className="absolute bottom-0 right-0 w-6">{subIcon}</div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-6 ml-4 lg:ml-0 lg:mt-6">
+        <h3 className="text-2xl font-bold text-text">{title}</h3>
+        <p className="text-gray-600">{text}</p>
+      </div>
+    </div>
+  );
+}
